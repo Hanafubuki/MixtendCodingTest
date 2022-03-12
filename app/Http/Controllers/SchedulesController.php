e171be989e9d01dc0fe17274ee101fb351e948ca
Hanafubuki <micheleweberr@gmail.com>
2022-03-12 20:10:55 +0900

    Update schedule log

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Resources\WorkingHourResource;
 use App\Models\Meeting;
 use App\Models\WorkingHour;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class SchedulesController extends Controller
 {
-    public function get(){
-        
+    public function get()
+    {        
         $working_hours = WorkingHour::select('start', 'end')->find(1);
         $meetings = Meeting::select('summary', 'date','start', 'end', 'timezone')
                             ->get()
@@ -18,9 +19,12 @@ class SchedulesController extends Controller
                             ->makeHidden('date')
                             ->toArray();     
         $data = ['working_hours' => new WorkingHourResource($working_hours),'meetings' => $meetings];
-
-        Log::info(print_r($data, true));
-
+        $this->log($data);
         return response()->json($data);
+    }
+
+    private function log($data){
+        Log::info(request()->ip());
+        Log::info(print_r($data, true));
     }
 }
